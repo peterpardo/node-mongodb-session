23f70f1c019fa35d57b0bcf5148ae25281fe6be8
Peter Pardo <peterpardo123@gmail.com>
2024-11-02 20:23:48 +0800

    Added create user function

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const UserModel = require("../User");
 const router = express.Router();
 
@@ -6,8 +7,24 @@ router.get("/users", (req, res) => {
   res.json({ users: [] });
 });
 
-router.post("/users", (req, res) => {
-  res.json({ message: "User created." });
+router.post("/users", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const saltRounds = parseInt(process.env.SALT_ROUNDS);
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const user = new UserModel({
+      email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    res.json({ message: "User created.", data: user });
+  } catch (err) {
+    console.error("Error creating user: ", err.message);
+    res.json({ message: "Error creating user..." });
+  }
 });
 
 router.patch("/users/:id", (req, res) => {
