27240fceeb9463fdae6ccfd695c6f8c911af3539
Peter Pardo <peterpardo123@gmail.com>
2024-11-06 19:10:35 +0800

    Added login functionality

--- index.js
+++ index.js
@@ -1,21 +1,69 @@
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const MongoDBStore = require("connect-mongodb-session")(session);
 const mongoose = require("mongoose");
 const routes = require("./routes/routes");
+const UserModel = require("./User");
+
 const app = express();
+const store = new MongoDBStore({
+  uri: "mongodb://localhost:27017/mydb",
+  collection: "mySessions",
+});
 
 const MONGO_STRING = process.env.DATABASE_URL;
 const PORT = process.env.PORT;
+
+store.on("error", (error) => {
+  console.log(error);
+});
+
+app.use(
+  session({
+    secret: "secret_key",
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 },
+    store: store,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 mongoose.connect(MONGO_STRING).then(() => console.log("DB connected"));
 
 app.use(express.json());
 
-app.use("/api", routes);
-
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.json({ session: req.session });
 });
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email });
+
+    if (user) {
+      const match = await bcrypt.compare(password, user.password);
+
+      if (match) {
+        req.session.regenerate((err) => {
+          if (err) {
+            throw new Error(err);
+          }
+          req.session.email = user.email;
+          res.json({ message: "Login success." });
+        });
+      } else {
+        res.json({ message: "Invalid credentials" });
+      }
+    }
+  } catch (err) {
+    res.json({ message: err.message });
+  }
+});
+
+app.use("/api", routes);
 
 app.listen(PORT, () => {
   console.log(`Server is running at port:${PORT}`);
